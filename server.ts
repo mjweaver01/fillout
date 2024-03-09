112da40317b323884c1c2a8319fd8d6a8049180b
electric-mike <66960109+electric-mike@users.noreply.github.com>
2024-03-09 18:19:09 -0500

    filtering working

--- server.ts
+++ server.ts
@@ -5,18 +5,32 @@ import data from './demo_data.json'
 
 const demoData: FilteredSubmissionsResponse = JSON.parse(JSON.stringify(data))
 
-const compareValues = (left: string | number, comparison: Condition, right: string | number) => {
-  if (!left || !comparison || !right) return
+const isDate = (date: string | number) => {
+  return (new Date(date) as any) !== 'Invalid Date' && !isNaN(new Date(date) as any)
+}
 
-  if (comparison === 'equals') {
-    return left == right
-  } else if (comparison === 'does_not_equal') {
-    return left != right
-  } else if (comparison === 'greater_than') {
-    return left >= right
-  } else if (comparison === 'less_than') {
-    return left < right
-  }
+const isNumeric = (value: string) => {
+  return /^-?\d+$/.test(value)
+}
+
+const compareValues = (left: string | number, comparison: Condition, right: string | number) => {
+  if (!left || !comparison || !right) return false
+
+  const l = isDate(left)
+    ? new Date(left).getTime()
+    : isNumeric(left.toString())
+      ? parseInt(left.toString())
+      : left
+  const r = isDate(right)
+    ? new Date(right).getTime()
+    : isNumeric(right.toString())
+      ? parseInt(right.toString())
+      : right
+
+  if (comparison === 'equals') return l == r
+  if (comparison === 'does_not_equal') return l != r
+  if (comparison === 'greater_than') return l > r
+  if (comparison === 'less_than') return l < r
 
   return false
 }
@@ -30,24 +44,30 @@ const filterResponses = (submissions: Submissions, query: any) => {
           `🔍 Filtering ${submissions.length} record${submissions.length != 1 ? 's' : ''} based on ${parsedFilters.length} filter${parsedFilters.length != 1 ? 's' : ''} `,
         ),
     )
-    if (parsedFilters.length) {
+    if (parsedFilters.length > 0) {
       let filteredSubmissions = submissions
         .map((submission) => {
-          let filteredQuestions = submission.questions
-            .map((question) => {
-              let filterChecks = parsedFilters
-                .map((f) => {
-                  return compareValues(question.value, f.condition, f.value) ? f : null
+          let matchedFilters = parsedFilters
+            .map((filter) => {
+              let matchedQuestions = submission.questions
+                .map((question) => {
+                  if (filter.id === question.id || filter.id === question.type) {
+                    if (compareValues(question.value, filter.condition, filter.value)) {
+                      return question
+                    }
+                  }
+
+                  return null
                 })
-                .filter((v) => v)
+                .filter((q) => q)
 
-              return filterChecks.length == parsedFilters.length ? question : null
+              return matchedQuestions.length > 0
             })
-            .filter((v) => v)
+            .filter((f) => !!f)
 
-          return filteredQuestions.length >= parsedFilters.length ? submission : null
+          return matchedFilters.length === parsedFilters.length ? submission : null
         })
-        .filter((v) => v)
+        .filter((f) => !!f)
 
       console.log(
         appName +
